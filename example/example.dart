2410a778e789496001e441f1c9731f72e622f025
Arnold Parge <pargearnold@gmail.com>
2021-11-08 13:02:29 +0530

    Updated example as Builder widget was no longer required

--- example/example.dart
+++ example/example.dart
@@ -27,52 +27,48 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Hawk Fab Menu Demo'),
       ),
-      body: Builder(
-        // builder is used only for the snackbar, if you don't want the snackbar you can remove
-        // Builder from the tree
-        builder: (BuildContext context) => HawkFabMenu(
-          icon: AnimatedIcons.menu_arrow,
-          fabColor: Colors.yellow,
-          iconColor: Colors.green,
-          items: [
-            HawkFabMenuItem(
-              label: 'Menu 1',
-              ontap: () {
-                ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Menu 1 selected')),
-                );
-              },
-              icon: const Icon(Icons.home),
-              color: Colors.red,
-              labelColor: Colors.blue,
-            ),
-            HawkFabMenuItem(
-              label: 'Menu 2',
-              ontap: () {
-                ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Menu 2 selected')),
-                );
-              },
-              icon: const Icon(Icons.comment),
-              labelColor: Colors.white,
-              labelBackgroundColor: Colors.blue,
-            ),
-            HawkFabMenuItem(
-              label: 'Menu 3 (default)',
-              ontap: () {
-                ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Menu 3 selected')),
-                );
-              },
-              icon: const Icon(Icons.add_a_photo),
-            ),
-          ],
-          body: const Center(
-            child: Text('Center of the screen'),
+      body: HawkFabMenu(
+        icon: AnimatedIcons.menu_arrow,
+        fabColor: Colors.yellow,
+        iconColor: Colors.green,
+        items: [
+          HawkFabMenuItem(
+            label: 'Menu 1',
+            ontap: () {
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Menu 1 selected')),
+              );
+            },
+            icon: const Icon(Icons.home),
+            color: Colors.red,
+            labelColor: Colors.blue,
           ),
+          HawkFabMenuItem(
+            label: 'Menu 2',
+            ontap: () {
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Menu 2 selected')),
+              );
+            },
+            icon: const Icon(Icons.comment),
+            labelColor: Colors.white,
+            labelBackgroundColor: Colors.blue,
+          ),
+          HawkFabMenuItem(
+            label: 'Menu 3 (default)',
+            ontap: () {
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Menu 3 selected')),
+              );
+            },
+            icon: const Icon(Icons.add_a_photo),
+          ),
+        ],
+        body: const Center(
+          child: Text('Center of the screen'),
         ),
       ),
     );
